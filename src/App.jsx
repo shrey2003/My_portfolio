dc1729b4645bc31ab8f03387e862e5b10ca54b73
shrey2003 <59368657+shrey2003@users.noreply.github.com>
2023-07-27 18:01:08 +0000

    Updated details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shreya Mishra",
+  title: "Tech Enthusiast",
+  email: "shreymishra0307@gmail.com",
+  gitHub: "https://github.com/shrey2003",
+  linkedIn: "https://www.linkedin.com/in/shreya-mishra03/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
